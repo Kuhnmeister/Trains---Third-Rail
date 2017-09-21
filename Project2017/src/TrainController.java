de0c08dfb816bd44302f7740f698c7112a659433
Yincheng He <yih39@pitt.edu>
2017-09-21 18:32:03 +0800

    changed ui

--- Project2017/src/TrainController.java
+++ Project2017/src/TrainController.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
 /*
     Created by yih on 09/16/2017
@@ -16,17 +17,34 @@ public class TrainController {
     private JLabel safeText;
     private JButton emergencyStopButton;
     private JButton brakeButton;
-    private JComboBox comboBox1;
+    private JSlider accSlider;
+    private JSlider brakeSlider;
+    private JButton accelerateButton;
     boolean isAutoMode = false;
+    static final int DEFAULT = 50;
 
     public TrainController() {
+        String[] Trains = {"Train 1", "Train 2", "Train 3"};
+        trainSelector.addItem("Train 1");
+        trainSelector.addItem("Train 2");
+        Hashtable labelTable = new Hashtable();
+        labelTable.put( new Integer( 0 ), new JLabel("Min") );
+        labelTable.put( new Integer( 50 ), new JLabel("Default") );
+        labelTable.put( new Integer( 100 ), new JLabel("Max") );
+        accSlider.setLabelTable( labelTable );
+        brakeSlider.setLabelTable( labelTable );
         autoMode.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 isAutoMode = true;
                 speedInput.setEnabled(false);
                 speedOK.setEnabled(false);
+                accSlider.setEnabled(false);
+                brakeSlider.setEnabled(false);
+                accSlider.setValue(DEFAULT);
+                brakeSlider.setValue(DEFAULT);
                 brakeButton.setEnabled(false);
+                accelerateButton.setEnabled(false);
             }
         });
         manualMode.addActionListener(new ActionListener() {
@@ -35,7 +53,10 @@ public class TrainController {
                 isAutoMode = false;
                 speedInput.setEnabled(true);
                 speedOK.setEnabled(true);
+                accSlider.setEnabled(true);
+                brakeSlider.setEnabled(true);
                 brakeButton.setEnabled(true);
+                accelerateButton.setEnabled(true);
             }
         });
     }
