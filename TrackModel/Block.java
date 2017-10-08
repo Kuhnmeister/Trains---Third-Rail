8917a2bd53d476fe7788d37669c6c9919aa93d0c
Zach Blouse <zachblouse@gmail.com>
2017-10-07 20:52:08 -0400

    Track Model Updates
    Can now load a hardcoded file into an arraylist of blocks.

--- TrackModel/Block.java
+++ TrackModel/Block.java
@@ -5,27 +5,87 @@ public class Block{
     private int length;
     private float grade;
     private int speedLimit;
-    private boolean isUnderground;
     private String infraStructure;
     private boolean isUnderground;
+    private String stationName;
+    private boolean isStation;
     private boolean hasSwitch;
     private int switchPosition;
+    private boolean hasRailwayCrossing;
 
-    public Block(int newBlockNum, int newLength, int newSomething){
-            blockNum=newBlockNum;
-            length=newLength;
-            something=newSomething;
+
+    public Block(String newLine,char newSection, int newBlockNum, int newLength, float newGrade, int newSpeedLimit,String newInfrastructure){
+        line = newLine;
+        section=newSection;
+        blockNum=newBlockNum;
+        length=newLength;
+        grade=newGrade;
+        speedLimit=newSpeedLimit;
+        if(newInfrastructure.contains("Underground")){
+            isUnderground=true;
+        }else{
+            isUnderground=false;
+        }
+        if(newInfrastructure.contains("Station")){
+            isStation=true;
+            String[] tempString=newInfrastructure.split(";");
+            stationName=tempString[1];
+        }else{
+            isStation=false;
+            stationName="No Station";
+        }
+        if(newInfrastructure.contains("Crossing")){
+            hasRailwayCrossing=true;
+        }else{
+            hasRailwayCrossing=false;
+        }
+        if(newInfrastructure.contains("Switch")){
+            hasSwitch=true;
+        }else{
+            hasSwitch=false;
+        }
     }
-
-
 
     public int GetBlockNum(){
         return blockNum;
     }
+
     public int GetLength(){
         return length;
     }
-    public int GetSomething(){
-        return something;
+
+    public String GetLine(){
+        return line;
+    }
+
+    public char GetSection(){
+        return section;
+    }
+
+    public float GetGrade(){
+        return grade;
+    }
+
+    public int GetSpeedLimit(){
+        return speedLimit;
+    }
+
+    public boolean GetIsUnderground(){
+        return isUnderground;
+    }
+    public String GetStationName(){
+        return stationName;
+    }
+    public boolean GetIsStation(){
+        return isStation;
+    }
+    public boolean GetHasSwitch(){
+        return hasSwitch;
+    }
+    public int GetSwitchPosition(){
+        return switchPosition;
+    }
+    public boolean GetHasRailwayCrossing(){
+        return hasRailwayCrossing;
     }
 }
--- TrackModel/TrackModel.java
+++ TrackModel/TrackModel.java
@@ -9,8 +9,8 @@ public class TrackModel {
     }
     private void Initialize(){
         LoadNewTrack();
-        Block newBlock = track.get(1);
-        System.out.println("Block Num: "+newBlock.GetBlockNum());
+        Block newBlock = track.get(2);
+        System.out.println("Underground: "+newBlock.GetIsUnderground()+"Grade: "+newBlock.GetGrade() + "Station: "+newBlock.GetStationName());
     }
     private void LoadNewTrack(){
         File f = new File("testTrack.txt");
@@ -22,7 +22,7 @@ public class TrackModel {
             String newLine;
             while((newLine = bufRead.readLine())!= null){
                 String[] blockString = newLine.split(",");
-                Block newBlock = new Block(Integer.parseInt(blockString[0]),Integer.parseInt(blockString[1]),Integer.parseInt(blockString[2]));
+                Block newBlock = new Block(blockString[0],blockString[1].charAt(0),Integer.parseInt(blockString[2]),Integer.parseInt(blockString[3]),Float.parseFloat(blockString[4]),Integer.parseInt(blockString[5]),blockString[6]);
                 track.add(newBlock);
             }
         }catch(IOException e){
@@ -31,3 +31,4 @@ public class TrackModel {
 
     }
 }
+//String newLine,char newSection, int newBlockNum, int newLength, float newGrade, int newSpeedLimit,String newInfrastructure
