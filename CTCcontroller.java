52e6e9873594481832258ab481affebcfe390f62
Kuhnmeister <31744903+Kuhnmeister@users.noreply.github.com>
2017-11-28 18:02:18 -0500

    Add files via upload

---
+++ CTCcontroller.java
@@ -0,0 +1,8 @@
+public class CTCcontroller{
+	public static void main(String[] args){
+		CTCcontroller thisController = new CTCcontroller();
+	}
+	public CTCcontroller(){
+		BBC thisBBC = new BBC();
+	}
+}
