531527e07c96653726e3da4782221e97be07f139
Kuhnmeister <31744903+Kuhnmeister@users.noreply.github.com>
2017-11-28 18:20:31 -0500

    Update CTCcontroller.java

--- CTCcontroller.java
+++ CTCcontroller.java
@@ -1,3 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.util.Timer;
+import java.util.List;
+
 public class CTCcontroller{
 	public static void main(String[] args){
 		CTCcontroller thisController = new CTCcontroller();
@@ -6,6 +16,7 @@ public class CTCcontroller{
 		BBC thisBBC = new BBC();
 	}
 	public void receiveTrackData(HashMap<String, HashMap<String, ArrayList<Block>>> track){
-		Tracking.receiveTrackData(track);
+		Tracking bob = new Tracking();
+		bob.receiveTrackData(track);
 	}
 }
