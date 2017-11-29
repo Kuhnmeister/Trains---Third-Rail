33b9a46c888e9979031e083c422b899341dda81a
Kuhnmeister <31744903+Kuhnmeister@users.noreply.github.com>
2017-11-28 19:47:39 -0500

    Update CTCcontroller.java

--- CTCcontroller.java
+++ CTCcontroller.java
@@ -15,6 +15,9 @@ public class CTCcontroller{
 	public CTCcontroller(Central central){
 		BBC thisBBC = new BBC(central);
 	}
+	public CTCcontroller(){
+		BBC thisBBC = new BBC();
+	}
 	public void receiveTrackData(HashMap<String, HashMap<String, ArrayList<Block>>> track){
 		Tracking bob = new Tracking();
 		bob.receiveTrackData(track);
